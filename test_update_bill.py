1a8440a5cc9bcdbe70c9c6de71b75a838077355b
DzHandsCode <codymichaeld@icloud.com>
2023-11-29 17:15:35 -0500

    tests the update_bill function

---
+++ test_update_bill.py
@@ -0,0 +1,20 @@
+import unittest
+from bill_tracker import BillTracker
+
+class TestRemoveBill(unittest.TestCase):
+    # Test suite for updating bill details in the BillTracker.
+    
+    def setUp(self):
+        # Set up a fresh instance of BillTracker for each test.
+        self.tracker = BillTracker()
+
+    def test_remove_bill(self):
+        # Test if a bill's details can be updated in the BillTracker.
+        # This test adds a bill, updates it, and then checks if the updated bill is present and the old bill is absent.
+        bill = {'name': 'Cable', 'amount': 20, 'due_date': '2023-05-25'}
+        self.tracker.add_bill(bill)
+        self.tracker.remove_bill(bill)
+        self.assertNotIn(bill, self.tracker.get_bills())
+
+if __name__ == '__main__':
+    unittest.main()
