ab072c93f96cd96c0154c1b747a3d2512b6935bd
DzHandsCode <codymichaeld@icloud.com>
2023-11-29 16:37:28 -0500

    tests the add_bill function

---
+++ test_add_bill.py
@@ -0,0 +1,19 @@
+import unittest
+from bill_tracker import BillTracker
+
+class TestAddBill(unittest.TestCase):
+    # Test suite for adding bills to the BillTracker.
+    
+    def setUp(self):
+        # Set up a fresh instance of BillTracker for each test.
+        self.tracker = BillTracker()
+
+    def test_add_bill(self):
+        # Test if a new bill can be added to the BillTracker.
+        # It verifies that the bill is correctly added by checking its presence in the list of bills.
+        bill = {'name': 'Electricity', 'amount': 100, 'due_date': '2023-05-20'}
+        self.tracker.add_bill(bill)
+        self.assertIn(bill, self.tracker.get_bills())
+
+if __name__ == '__main__':
+    unittest.main()
