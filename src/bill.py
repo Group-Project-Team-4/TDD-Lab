1b904b2608ff526fc0bde59b55277f3e277c2b39
Cody Dietrich <codymichaeld@icloud.com>
2023-12-04 02:55:47 -0500

    commented out all source code for lab start

--- src/bill.py
+++ src/bill.py
@@ -1,19 +1,19 @@
-class Bill:
-    # A class representing a single bill.
+# class Bill:
+#     # A class representing a single bill.
 
-    def __init__(self, name, amount, due_date):
-        """
-        Initialize a new Bill instance.
+#     def __init__(self, name, amount, due_date):
+#         """
+#         Initialize a new Bill instance.
 
-        Args:
-            name (str): The name of the bill.
-            amount (float): The amount due for the bill.
-            due_date (str): The due date of the bill.
-        """
-        self.name = name
-        self.amount = amount
-        self.due_date = due_date
+#         Args:
+#             name (str): The name of the bill.
+#             amount (float): The amount due for the bill.
+#             due_date (str): The due date of the bill.
+#         """
+#         self.name = name
+#         self.amount = amount
+#         self.due_date = due_date
 
-    def __repr__(self):
-        # A string representation of a Bill instance.
-        return f"Bill(name='{self.name}', amount={self.amount:.2f}, due_date='{self.due_date}')"
+#     def __repr__(self):
+#         # A string representation of a Bill instance.
+#         return f"Bill(name='{self.name}', amount={self.amount:.2f}, due_date='{self.due_date}')"
--- src/bill_tracker.py
+++ src/bill_tracker.py
@@ -1,32 +1,30 @@
-from src.bill import Bill
+# class BillTracker:
+#     # A class to track monthly bills.
 
-class BillTracker:
-    # A class to track monthly bills.
+#     def __init__(self):
+#         # Initialize the bill tracker with an empty list of bills.
+#         self.bills = []
 
-    def __init__(self):
-        # Initialize the bill tracker with an empty list of bills.
-        self.bills = []
+#     def add_bill(self, bill):
+#         # Add a bill to the tracker.
+#         self.bills.append(bill)
 
-    def add_bill(self, bill):
-        # Add a bill to the tracker.
-        self.bills.append(bill)
+#     def get_bills(self):
+#         # Return a list of all bills.
+#         return self.bills
 
-    def get_bills(self):
-        # Return a list of all bills.
-        return self.bills
+#     def calculate_total(self):
+#         # Calculate the total amount of all bills.
+#         return sum(bill.amount for bill in self.bills)
 
-    def calculate_total(self):
-        # Calculate the total amount of all bills.
-        return sum(bill.amount for bill in self.bills)
+#     def remove_bill(self, bill):
+#         # Remove a bill from the tracker.
+#         bill_to_remove = bill
+#         self.bills = [bill for bill in self.bills if bill != bill_to_remove]
 
-    def remove_bill(self, bill):
-        # Remove a bill from the tracker.
-        bill_to_remove = bill
-        self.bills = [bill for bill in self.bills if bill != bill_to_remove]
-
-    def update_bill(self, old_bill, new_bill):
-        # Update the details of a bill.
-        for i, bill in enumerate(self.bills):
-            if bill == old_bill:
-                self.bills[i] = new_bill
-                break
+#     def update_bill(self, old_bill, new_bill):
+#         # Update the details of a bill.
+#         for i, bill in enumerate(self.bills):
+#             if bill == old_bill:
+#                 self.bills[i] = new_bill
+#                 break
