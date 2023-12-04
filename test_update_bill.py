e1fa2e60b738fdf1aecdcb29be5aeb0a21aa6d55
Cody Dietrich <codymichaeld@icloud.com>
2023-12-04 00:47:29 -0500

    refactored to use Bill class

--- test_update_bill.py
+++ test_update_bill.py
@@ -1,5 +1,6 @@
 import unittest
 from bill_tracker import BillTracker
+from bill import Bill
 
 class TestRemoveBill(unittest.TestCase):
     # Test suite for updating bill details in the BillTracker.
@@ -11,7 +12,7 @@ class TestRemoveBill(unittest.TestCase):
     def test_remove_bill(self):
         # Test if a bill's details can be updated in the BillTracker.
         # This test adds a bill, updates it, and then checks if the updated bill is present and the old bill is absent.
-        bill = {'name': 'Cable', 'amount': 20, 'due_date': '2023-05-25'}
+        bill = Bill('Cable', 20, '2023-05-25')
         self.tracker.add_bill(bill)
         self.tracker.remove_bill(bill)
         self.assertNotIn(bill, self.tracker.get_bills())
